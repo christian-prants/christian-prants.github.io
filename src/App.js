ff3092f0763c9830ca3667e3837b3fca9aaa2842
christian-prants <chris.prants9@gmail.com>
2022-11-27 19:54:05 -0300

    corrected the path for about page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path = "/" element = { <About /> } />
+        <Route exact path = "/" element = { <About /> } />
         <Route path = "/projects" element = { <Projects /> } />
       </Routes>
       <Footer />
--- src/screens/header.js
+++ src/screens/header.js
@@ -6,7 +6,7 @@ import '../styles/header.scss';
 const Header = () => {
     return (
         <div className = 'main'>
-            <NavLink to = '/'  className = 'sobre'  activeclassname = 'selected'>sobre</NavLink>
+            <NavLink exact to = '/'  className = 'sobre'  activeclassname = 'selected'>sobre</NavLink>
             <NavLink to = '/projects'  className = 'projetos'  activeclassname = 'selected'>projetos</NavLink>
             <a href="https://www.linkedin.com/in/christian-prants/" className = 'linkedin' target="_blank" rel="noopener noreferrer">
                 linkedin
